670219440ff2e5f4aaf6890fb13f1f39167025e6
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-07-31 20:20:17 -0700

    Add Mocha test

---
+++ 3-Automating Tests with Mocha/index_text.js
@@ -0,0 +1,12 @@
+// Testing Math object...
+describe('Math', () => {
+  // ..with .min method
+  describe('.min', () => {
+    it('returns the argument with the lowest value', () => {
+      // Your test goes here
+    });
+    it('returns -Infinity when no arguments are provided', () => {
+      // Your test goes here
+    });
+  });
+});
