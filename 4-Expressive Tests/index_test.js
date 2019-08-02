1c02f1554d659f43748dffc27aaeba506c17e405
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-08-01 18:02:47 -0700

    Add deepEqual test

--- 4-Expressive Tests/index_test.js
+++ 4-Expressive Tests/index_test.js
@@ -51,3 +51,18 @@ describe('-', () => {
     assert.strictEqual(result, expected);
   });
 });
+
+// TEST 5 - assert.deepEqual
+describe('+', () => {
+  it('returns the sum of two values', () => {
+    // Setup
+    let expected = { a: 3, b: 4, result: 7 };
+    let sum = { a: 3, b: 4 };
+
+    // Exercise
+    sum.result = sum.a + sum.b;
+
+    // Verify
+    assert.deepEqual(sum, expected);
+  });
+});
