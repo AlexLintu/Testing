f7dfac8e88a30aa4c97b52396bba902ddb156cd5
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-08-01 18:05:10 -0700

    Use deepEqual on arrays

--- 4-Expressive Tests/index_test.js
+++ 4-Expressive Tests/index_test.js
@@ -66,3 +66,18 @@ describe('+', () => {
     assert.deepEqual(sum, expected);
   });
 });
+
+// Arrays
+describe('+', () => {
+  it('returns the sum of two values', () => {
+    // Setup
+    let expected = [3, 4, 7];
+    let sum = [3, 4];
+
+    // Exercise
+    sum.push(3 + 4);
+
+    // Verify
+    assert.deepEqual(sum, expected);
+  });
+});
