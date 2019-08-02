375a0e8ac307a3e2585a46fd9e057a857e12fef9
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-08-01 17:41:36 -0700

    Use assert.equal

--- 4-Expressive Tests/index_test.js
+++ 4-Expressive Tests/index_test.js
@@ -1,9 +1,39 @@
+// Import of the assert Node module
 const assert = require('assert');
 
+// TEST 1
 describe('+', () => {
   it('returns the sum of its arguments', () => {
 
     assert.ok(3 + 4 === 7);
 
+  });
+});
+
+// TEST 2 - assert.ok
+describe('-', () => {
+  it('returns the difference of two values', () => {
+    const bigNum = 100;
+    const smallNum = 4;
+    const expected = 96;
+
+    const result = bigNum - smallNum;
+
+    // Write assertion here
+    assert.ok(result === expected);
+  });
+});
+
+// TEST 3 - assert.equal
+describe('-', () => {
+  it('returns the difference of two values', () => {
+    const bigNum = 100;
+    const smallNum = 4;
+    const expected = 96;
+
+    const result = bigNum - smallNum;
+
+    // Write assertion here
+    assert.equal(result, expected);
   });
 });
