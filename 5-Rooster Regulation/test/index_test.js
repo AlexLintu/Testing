7b7b93b67a79226367b83cbe96308539b564d562
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-08-03 16:03:11 -0700

    Wrote first test

--- 5-Rooster Regulation/test/index_test.js
+++ 5-Rooster Regulation/test/index_test.js
@@ -3,6 +3,16 @@ const Rooster = require('../index');
 
 describe('Rooster', () => {
   describe('.announceDawn', () => {
+    it('returns a rooster call', () => {
+      // 1. Setup
+      const expected = 'cock-a-doodle-doo!';
+      // 2. Exercise
+      // Call Rooster.announceDawn and store result in variable
+      const result = Rooster.announceDawn();
 
+      // 3. Verify
+      // Use an assert method to compare actual and expected result
+      assert.equal(result, expected);
+    })
   })
 });
