7c55931afc8e462515a2e1c68216dafb6d1de6dd
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-08-03 16:04:11 -0700

    Fix comments

--- 5-Rooster Regulation/test/index_test.js
+++ 5-Rooster Regulation/test/index_test.js
@@ -6,12 +6,10 @@ describe('Rooster', () => {
     it('returns a rooster call', () => {
       // 1. Setup
       const expected = 'cock-a-doodle-doo!';
-      // 2. Exercise
-      // Call Rooster.announceDawn and store result in variable
+      // 2. Exercise - call Rooster.announceDawn and store result in variable
       const result = Rooster.announceDawn();
 
-      // 3. Verify
-      // Use an assert method to compare actual and expected result
+      // 3. Verify - use an assert method to compare actual and expected result
       assert.equal(result, expected);
     })
   })
