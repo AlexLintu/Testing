b54eee78dea747b6613a4b05323a1de73b0a69d3
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-08-07 14:44:07 -0700

    Add second test

--- 6-TDD/index_test.js
+++ 6-TDD/index_test.js
@@ -3,6 +3,8 @@ const Calculate = require('../index.js')
 
 describe('Calculate', () => {
   describe('.sum', () => {
+
+    // First Test
     it('returns the sum of an array of numbers', () => {
       // Setup
       const inputArr = [1, 2, 3];
@@ -12,5 +14,16 @@ describe('Calculate', () => {
       // Verification
       assert.equal(result, expectedSum);
     });
+
+    // Second Test
+    it('returns the sum of a four-item list', () => {
+      // Setup
+      const inputArr = [4, 5, 6, 7];
+      const expectedSum = 22;
+      // Exercise
+      const result = Calculate.sum(inputArr);
+      // Verification
+      assert.equal(result, expectedSum);
+    })
   });
 });
