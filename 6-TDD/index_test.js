f454dc0e78f501d72909510a99c8ec41c7ff1589
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-08-06 19:33:16 -0700

    Refactor the test

--- 6-TDD/index_test.js
+++ 6-TDD/index_test.js
@@ -4,7 +4,14 @@ const Calculate = require('../index.js')
 describe('Calculate', () => {
   describe('.sum', () => {
     it('returns the sum of an array of numbers', () => {
-      assert.equal(Calculate.sum([1, 2, 3]), 6);
+
+      // Setup
+      const inputArr = [1, 2, 3];
+      const expectedSum = 6;
+      // Exercise
+      const result = Calculate.sum(inputArr);
+      // Verification
+      assert.equal(result, expectedSum);
     });
   });
 });
