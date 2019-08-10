55cd04af4a06fd2ca795b3ebabcbd300745796a9
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-08-09 18:18:50 -0700

    Refactor the test

--- 7-JS Unit Tetsing/index_test.js
+++ 7-JS Unit Tetsing/index_test.js
@@ -4,7 +4,11 @@ var Calculate = require('../index.js')
 describe('Calculate', () => {
   describe('.factorial', () => {
     it('tests if the output of !5 is equal to 120', () => {
-      assert.equal(Calculate.factorial(5), 120);
+      const expectedResult = 120;
+      const inputNum = 5;
+
+      const result = Calculate.factorial(inputNum);
+      assert.equal(result, expectedResult);
     });
   });
 });
